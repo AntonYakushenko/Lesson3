f39216753576b70d954fb1fa27e18c4a32f116d2
antonyakusenko <mnbv0@rambler.ru>
2021-09-09 03:50:55 +0400

    add String.format

--- src/main/java/ITCompany.java
+++ src/main/java/ITCompany.java
@@ -1,25 +1,17 @@
 public class ITCompany {
     public static void main(String[] args) {
+    Role qa = new Role(40000, "Middle", "Low", "QA", 2, false);
+    Role programmer = new Role(200000, "Senior", "High softskills", "Programmer", 5, true);
 
+    String textQA = String.format("Employee information. salary: %d, qualification: %s, communication: %s, position: %s , работает из офиса: %b .",
+                qa.salary, qa.qualification, qa.communication, qa.position,qa.workfromoffce);
+    String textProgrammer = String.format("Employee information. salary: %d, qualification: %s, communication: %s, position: %s , работает из офиса: %b .",
+                programmer.salary, programmer.qualification, programmer.communication, programmer.position,programmer.workfromoffce);
 
-    Role qa = new Role(40000,
-                       "Middle",
-                       "Low",
-                       "QA",
-                       2,
-                       false);
+        System.out.println(textQA);
+        System.out.println(textProgrammer);
 
-    Role programmer = new Role (200000,
-            "Senior",
-            "High softskills",
-            "Programmer",
-            5,
-            true);
-
-
-        System.out.println(qa.salary + "" + qa.experience);
-        System.out.println(programmer.salary + " " + programmer.communication);
-
-
+       /* System.out.println(qa.salary + "" + qa.experience);
+        System.out.println(programmer.salary + " " + programmer.communication);*/
     }
 }
